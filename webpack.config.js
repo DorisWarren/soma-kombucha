7871d8d1211e39d958e7d0f5ebb4933494fbf68e
Doris Warren <doriswarren11@gmail.com>
2019-04-19 16:52:49 -0700

    add title to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -88,7 +88,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template:'template.ejs',
       appMountId: 'react-app-root',
-      title: 'React Help Queue',
+      title: 'Soma Kombucha Taproom',
       filename: resolve(__dirname, "build", "index.html"),
     }),
   ]

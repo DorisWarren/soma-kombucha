c5e8938c353c860fb45040ed9dc864672872de40
Doris Warren <doriswarren11@gmail.com>
2019-04-19 12:32:23 -0700

    doing research on routing-reviewing this weeks material

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import soma from '../assets/images/soma.jpeg';
 function Header(){
   return (
     <div>
-    <img style={{width:'70%'}} src={soma}/>
+    <img style={{width:'70%', position:'relative', marginLeft: '155px'}} src={soma}/>
       <style jsx>{`
         
         `}</style>

5ad9b00288618ce0f00ad51efee15523c5916623
Doris Warren <doriswarren11@gmail.com>
2019-04-19 15:05:02 -0700

    removed anchor router- figured out how to link it correctly with routerreactdom

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import soma from '../assets/images/soma.jpeg';
 import { Link } from 'react-router-dom';
-import AnchorLink from 'react-anchor-link-smooth-scroll'
+
 
 function Header(){
   return (

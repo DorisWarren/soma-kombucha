aee9f133a7043bbfcdcc6e4d9953cd24f01c8ab6
Doris Warren <doriswarren11@gmail.com>
2019-04-26 16:26:23 -0700

    add lift state to FlavorList

--- src/components/FlavorList.jsx
+++ src/components/FlavorList.jsx
@@ -1,29 +1,11 @@
 import React from 'react';
-import Ticket from './Ticket';
+import Day from './Day';
 
-var masterTicketList = [
-  {
-    names: 'Thato and Haley',
-    location: '3A',
-    issue: 'Firebase won\'t save record. Halp.'
-  },
-  {
-    names: 'Sleater and Kinney',
-    location: '4B',
-    issue: 'Fox image not displaying on page, can only see duck?'
-  },
-  {
-    names: 'Imani & Jacob',
-    location: '9F',
-    issue: 'Donkey picture not displaying on hover in Zoology app. :('
-  }
-];
-
-function TicketList(){
+function FlavorList(){
   return (
     <div>
       <hr/>
-      {masterTicketList.map((ticket, index) =>
+      {masterSomaFlavor.map((ticket, index) =>
         <Ticket names={ticket.names}
           location={ticket.location}
           issue={ticket.issue}
@@ -33,4 +15,4 @@ function TicketList(){
   );
 }
 
-export default TicketList;
+export default FlavorList;
